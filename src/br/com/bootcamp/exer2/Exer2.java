9de8536b2a75d03c54e95395f13db599434d3f4f
RonaldoJasckowDeCampos <ronaldo_ela@outlook.com>
2021-02-01 15:07:42 -0300

    exer2

---
+++ src/br/com/bootcamp/exer2/Exer2.java
@@ -0,0 +1,13 @@
+package br.com.bootcamp.exer2;
+
+import br.com.bootcamp.exer2.calculadora.Calculadora;
+
+public class Exer2 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        System.out.println(calculadora.somar(2, 2));
+        System.out.println(calculadora.subtrair(2, 2));
+        System.out.println(calculadora.multiplicar(2, 2));
+        System.out.println(calculadora.dividir(5, 2));
+    }
+}
